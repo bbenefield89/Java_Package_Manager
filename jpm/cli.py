9dbc46ff085ed49aca1d7c656b9b11b7fbf4d348
Brandon Benefield <bsquared18@gmail.com>
2019-06-15 17:37:35 -0400

    CLI WORKING
    - bin/jpm
      - responsible for executing the CLI
    
    - jpm/cli.py
      - holds all the actual calls that make the CLI work
    
    - setup.py
      - update patch version

---
+++ jpm/cli.py
@@ -0,0 +1,3 @@
+def main():
+    print('Hello, World')
+    print('Welcome to JPM the unofficial Java Package Manager')
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Java Package Manager",
-    version="0.0.2",
+    version="0.0.5",
     author="Author Name",
     author_email="bsquared18@gmail.com",
     description="A small CLI package manager for Java projects using Maven",
@@ -17,5 +17,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    scripts=[
+        'bin/jpm'
     ],
 )
