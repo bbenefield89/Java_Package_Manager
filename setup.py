fafee3703c95a935446142b2a5e8e3b8d8e27345
Brandon Benefield <bsquared18@gmail.com>
2019-06-15 15:27:55 -0400

    ADD PROJECT SETUP DETAILS
    - jpm/
      - change package name
    
    - setup.py
      - change package config details

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-your-username",
+    name="Java Package Manager",
     version="0.0.1",
     author="Author Name",
-    author_email="email_address@domain.com",
-    description="A small example package",
+    author_email="bsquared18@gmail.com",
+    description="A small CLI package manager for Java projects using Maven",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
